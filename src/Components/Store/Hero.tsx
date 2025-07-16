8a0c423aa3fd253fe06100f059a754c62b329dcf
Alisina <alisinasoltani82@gmail.com>
2025-07-16 17:13:58 +0330

    store ui: hero section complete

--- src/Components/Store/Hero.tsx
+++ src/Components/Store/Hero.tsx
@@ -4,13 +4,13 @@ import download_icon from "@/../public/icons/store/download.svg";
 
 const Hero = () => {
   return (
-    <div className="relative w-screen h-screen flex flex-row justify-center md:justify-end items-center md:items-start
+    <div className="relative w-screen h-[50vh] md:h-screen flex flex-row justify-center md:justify-end items-center md:items-start
     bg-gradient-to-l from-[#6B66FF] from-0% to-[#4B45EB] to-100%">
-        <div className="flex flex-col items-center md:items-start gap-3 text-white px-[5rem] py-[5rem] mb-[12rem]" style={{ direction: 'rtl' }}>
-            <h1 className="text-[3rem] md:text-[4rem] iransans_demi">
+        <div className="flex flex-col items-center md:items-start gap-3 text-white px-[2rem] md:px-[5rem] py-[5rem] mb-[12rem]" style={{ direction: 'rtl' }}>
+            <h1 className="text-[2rem] md:text-[4rem] iransans_demi">
                 کالا اندیش مایسا
             </h1>
-            <h2 className="max-w-[22rem] md:max-w-none text-[1.6rem] md:text-[2rem] iransans_regular text-center md:text-right">
+            <h2 className="max-w-[22rem] md:max-w-none text-[1.25rem] md:text-[2rem] iransans_regular text-center md:text-right">
                 رضایت شما در اولویت است یک شعار نیست!
             </h2>
             <button className="w-fit flex flex-row justify-center items-center gap-1 border-2 border-white px-3 py-2 rounded-xl mt-4">
@@ -20,7 +20,7 @@ const Hero = () => {
                 <Image src={download_icon} width={30} alt="Click to Download Products Catalog" />
             </button>
         </div>
-        <div className="absolute max-w-[600px] left-0 bottom-0">
+        <div className="absolute max-w-[400px] md:max-w-[600px] left-0 bottom-0">
             <Image src={hero_image} alt="کالا اندیش مایسا: نفس راحت برای همه!" />
         </div>
     </div>
